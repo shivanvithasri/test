f259b4c1bbdb90c95a90a9a34b45b3bc488ea0a9
shivanvithasri <102052348+shivanvithasri@users.noreply.github.com>
2024-06-04 09:02:13 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+module "secret-manager" {
+  source  = "GoogleCloudPlatform/secret-manager/google"
+  version = "~> 0.2"
+  project_id = "shivanvitha"
+  secrets = [
+    {
+      name                     = "secret-5"
+      automatic_replication    = true
+      secret_data              = "secret information"
+    }
+  ]
+}
